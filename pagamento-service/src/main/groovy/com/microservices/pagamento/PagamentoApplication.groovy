e7307b7f5289075e9963d7d119413014b5ac38a8
Rafael Carvalhedo <rafaelcarvalhedo.sousa@gmail.com>
2025-04-11 18:26:50 -0300

    +features

---
+++ pagamento-service/src/main/groovy/com/microservices/pagamento/PagamentoApplication.groovy
@@ -0,0 +1,11 @@
+package com.microservices.pagamento
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PagamentoApplication {
+    static void main(String[] args) {
+        SpringApplication.run(PagamentoApplication, args)
+    }
+} 
