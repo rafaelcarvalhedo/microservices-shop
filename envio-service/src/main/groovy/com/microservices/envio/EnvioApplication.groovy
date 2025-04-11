03e7782f1b500f0c39b381bb12f9e5faa851d116
Rafael Carvalhedo <rafaelcarvalhedo.sousa@gmail.com>
2025-04-11 18:01:47 -0300

    +features

---
+++ envio-service/src/main/groovy/com/microservices/envio/EnvioApplication.groovy
@@ -0,0 +1,11 @@
+package com.microservices.envio
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EnvioApplication {
+    static void main(String[] args) {
+        SpringApplication.run(EnvioApplication, args)
+    }
+} 
